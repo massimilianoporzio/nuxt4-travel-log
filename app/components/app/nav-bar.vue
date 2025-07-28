8d1926de7c8d068c6493ec3adf211045df0dd2da
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2025-07-28 18:47:40 +0200

    ok navbar daisyUI

---
+++ app/components/app/nav-bar.vue
@@ -0,0 +1,11 @@
+/*
+ *   Copyright (c) 2025 Massimiliano Porzio
+ *   All rights reserved.
+ */
+<template>
+  <div class="navbar bg-accent text-accent-content">
+    <button class="btn btn-ghost text-xl">
+      daisyUI
+    </button>
+  </div>
+</template>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,5 +1,10 @@
+/*
+ *   Copyright (c) 2025 Massimiliano Porzio
+ *   All rights reserved.
+ */
 <template>
   <div class="flex min-h-screen flex-col">
+    <app-nav-bar />
     <main>
       <slot />
     </main>
