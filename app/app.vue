8482638e15e8f31fa424e77b6a3badaaec1b8cd3
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2025-07-28 11:52:21 +0200

    setup layout / pages

--- app/app.vue
+++ app/app.vue
@@ -1,8 +1,5 @@
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  <button class="btn btn-secondary">
-    View Map <Icon name="tabler:map" />
-  </button>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
