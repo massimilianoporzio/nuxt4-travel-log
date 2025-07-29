5401d3b72b8d8232f1a3dc8a1d565bb6483bac4e
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2025-07-29 12:08:07 +0200

    toggle theme risolto

---
+++ app/components/app/theme-toggle.vue
@@ -0,0 +1,28 @@
+/*
+ *   Copyright (c) 2025 Massimiliano Porzio
+ *   All rights reserved.
+ */
+<script setup lang="ts">
+const colorMode = useColorMode();
+const isDark = computed({
+  get() {
+    return colorMode.value === "dark";
+  },
+  set(value) {
+    colorMode.preference = value ? "dark" : "emerald";
+  },
+});
+</script>
+
+<template>
+  <label class="swap swap-rotate mx-4">
+    <!-- this hidden checkbox controls the state -->
+    <input v-model="isDark" type="checkbox">
+
+    <!-- sun icon -->
+    <Icon name="tabler:sun" class="swap-on" size="24" />
+
+    <!-- moon icon -->
+    <Icon name="tabler:moon-filled" class="swap-off" size="24" />
+  </label>
+</template>
