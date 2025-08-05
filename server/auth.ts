09b6f0a814ccce20bb96549e5ef9bb6817dbce6e
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2025-08-05 18:40:04 +0200

    bozza di invio reset password ma va creata pagina di reset password

--- server/auth.ts
+++ server/auth.ts
@@ -2,14 +2,17 @@
  *   Copyright (c) 2025 Massimiliano Porzio
  *   All rights reserved.
  */
-import { betterAuth } from "better-auth";
+import { betterAuth, logger } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 
 import db from "./db/index"; // your drizzle instance
 import env from "./env";
 
 export const auth = betterAuth({
-  emailAndPassword: { enabled: true },
+  emailAndPassword: { enabled: true, sendResetPassword: async ({ user, url, token }) => {
+    // Send reset password email
+    logger.info(`Send reset password email to ${user.email} with token ${token} at ${url}`);
+  } },
   socialProviders: {
     github: {
       clientId: env.GITHUB_CLIENT_ID,
